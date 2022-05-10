949313586a41726f35b655c7c181f11480ac24e0
Konstantin Ligus <kostya1989com@gmail.com>
2022-05-10 16:15:21 +0300

    done ex1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+function galleryRender(galleryItems) {
+  refs.gallery.innerHTML = galleryItems.reduce(
+    (acc, { preview, original, description } = galleryItems) =>
+      acc +
+      `<a class="gallery__item" href=${original}>
+<img class="gallery__image" src="${preview}" alt=${description}/>
+</a>`,
+    '',
+  );
+}
+galleryRender(galleryItems);
+const galleryRide = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-console.log(galleryItems);
+// console.log(galleryItems);
